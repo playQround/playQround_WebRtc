c08370204775e25e56feffa9e8a40357b3f2a537
peterlah <58283954+peterlah@users.noreply.github.com>
2023-07-08 17:50:34 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.static('test'));
-  await app.listen(80);
+  await app.listen(3000);
 }
 bootstrap();
